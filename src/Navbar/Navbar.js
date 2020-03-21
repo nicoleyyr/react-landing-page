8813e0760cee93b31a6f8847f99094ec96934c0c
Nicole Yang <yoyotpe@hotmail.com>
2020-03-21 23:18:03 +0800

    add leaf images

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -2,10 +2,23 @@ import React from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
 
 const navbar = () => {
+	const style = {
+		color: "#e2d4a8",
+		fontWeight: "bold"
+	}
 	return (
 		<Navbar className="justify-content-between" fixed="top" bg="white">
 			<Container>
-				<Navbar.Brand href="#home">Test Hotel</Navbar.Brand>
+				<Navbar.Brand href="#home" style={style}>
+					<img
+						alt=""
+						src="/logo.png"
+						width="30"
+						height="30"
+						className="d-inline-block align-top"
+					/>{" "}
+					Test Hotel
+				</Navbar.Brand>
 				<Nav className="justify-content-end">
 					<Nav.Link href="#about-us">About us</Nav.Link>
 					<Nav.Link href="#rooms">Rooms</Nav.Link>
