38b841b3b1347f81b1fbdd4ab05e98d49f7b5776
Nicole <yoyotpe@hotmail.com>
2020-03-25 21:21:17 +0800

    Updates

---
+++ precache-manifest.dc59728620bc903c807850792290e152.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "399139f6f2d7a5fb2883374a0c1cbb7d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "cab4416fc61ad9c1e5eb",
+    "url": "./static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "fe2a65d9b0de9bc8e083",
+    "url": "./static/css/main.bae2a107.chunk.css"
+  },
+  {
+    "revision": "cab4416fc61ad9c1e5eb",
+    "url": "./static/js/2.597a3862.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "./static/js/2.597a3862.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe2a65d9b0de9bc8e083",
+    "url": "./static/js/main.e39f2784.chunk.js"
+  },
+  {
+    "revision": "18d292f81a532a21794f",
+    "url": "./static/js/runtime-main.46f2fa13.js"
+  }
+]);
