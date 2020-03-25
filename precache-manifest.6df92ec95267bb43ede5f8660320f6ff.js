1da7f6a9a83f564c3a5a846a98da90a77a9193f2
Nicole <yoyotpe@hotmail.com>
2020-03-25 20:28:51 +0800

    Updates

---
+++ precache-manifest.6df92ec95267bb43ede5f8660320f6ff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a063327a3372f3c9515c0466f296b24a",
+    "url": "/react-landing-page/index.html"
+  },
+  {
+    "revision": "7fa8310030b9549f7904",
+    "url": "/react-landing-page/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "43a8955052bccf4ba71e",
+    "url": "/react-landing-page/static/css/main.bae2a107.chunk.css"
+  },
+  {
+    "revision": "7fa8310030b9549f7904",
+    "url": "/react-landing-page/static/js/2.28c097ca.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-landing-page/static/js/2.28c097ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43a8955052bccf4ba71e",
+    "url": "/react-landing-page/static/js/main.c51f836a.chunk.js"
+  },
+  {
+    "revision": "5288efa17a2794acfff4",
+    "url": "/react-landing-page/static/js/runtime-main.8812d0c9.js"
+  }
+]);
